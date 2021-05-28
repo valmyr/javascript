8f9fc631e00c8770fa57ac690bc052d125f09195
valmyr <65004196+valmyr@users.noreply.github.com>
2021-05-28 09:31:44 -0300

    .

---
+++ aula.js
@@ -0,0 +1,10 @@
+// console.log("Valmir Ferreira da Silva")
+//variáveis
+// let idade = 54;
+// console.log(idade)
+let nome = "Valmir Ferreira da Silva";
+let idade = 21;
+let altura = 1.56
+console.log("Nome: "+nome);
+console.log("idade: "+idade);
+console.log("altura: "+altura);
