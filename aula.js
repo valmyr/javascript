965837009334ae9bda2ed534bdd8738552adeffc
valmyr <65004196+valmyr@users.noreply.github.com>
2021-05-28 09:47:28 -0300

    comandos básicos

--- aula.js
+++ aula.js
@@ -5,6 +5,29 @@
 let nome = "Valmir Ferreira da Silva";
 let idade = 21;
 let altura = 1.56
-console.log("Nome: "+nome);
-console.log("idade: "+idade);
-console.log("altura: "+altura);
+// console.log("Nome: "+nome);
+// console.log("idade: "+idade);
+// console.log("altura: "+altura);
+//objetos
+let pessoa = {
+    nome:'Valmir',
+    idade:21,
+    sobrenome:'Ferreira da Silva'
+
+};
+console.log(pessoa.nome)
+//arrays
+let numerosPrimos =[2,3,5,7,11,13];
+console.log(numerosPrimos)
+//funções
+function mostrarNome(){
+    console.log("Valmir")
+};
+function somarNumeros(n1,n2){
+    return n1+n2;
+};
+console.log(somarNumeros(9,3));
+function multiplicarPor2(numero){
+    return numero*2;
+};
+console.log(multiplicarPor2(7));
